8840f6aea023f068a6139036a9af477b4c1cc37d
Alexander Zimin <alexanderzi@123software.ru>
2019-09-12 11:35:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const execSemanticRelease = require('./semanticRelease');
+
+const main = async () => {
+  await execSemanticRelease();
+};
+
+main();
---
+++ src/semanticRelease.js
@@ -0,0 +1,38 @@
+const semanticRelease = require('semantic-release');
+
+const execSemanticRelease = async () => {
+  try {
+    const result = await semanticRelease({
+      // Core options
+      branch: 'master',
+      // repositoryUrl: 'https://github.com/me/my-package.git',
+      // Shareable config
+      // extends: 'my-shareable-config',
+      // Plugin options
+      // githubUrl: 'https://my-ghe.com',
+      // githubApiPathPrefix: '/api-prefix'
+    }, {
+      env: { ...process.env },
+    });
+  
+    if (result) {
+      const {lastRelease, commits, nextRelease, releases} = result;
+  
+      console.log(`Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`);
+  
+      if (lastRelease.version) {
+        console.log(`The last release was "${lastRelease.version}".`);
+      }
+  
+      for (const release of releases) {
+        console.log(`The release was published with plugin "${release.pluginName}".`);
+      }
+    } else {
+      console.log('No release published.');
+    }
+  } catch (err) {
+    console.error('The automated release failed with %O', err);
+  }
+};
+
+module.exports = execSemanticRelease;
